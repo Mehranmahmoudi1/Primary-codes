39806d2b1423b1590af3cfdcc0064cdd4d7ecd98
Mehranmahmoudi1 <149366701+Mehranmahmoudi1@users.noreply.github.com>
2023-11-11 19:08:17 +0330

    Update mehran.py

--- mehran.py
+++ mehran.py
@@ -2,3 +2,11 @@ x = 5
 y = "mehran"
 print(x)
 print(y)
+____________________
+x=int(int(input())
+print ("لطفایک عدد وارد کنید")
+if x/2==0:
+      print("zog")
+      else:
+print ("fard")
+____________________
